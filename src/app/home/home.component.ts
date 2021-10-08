519d8f2284067ef33f5d6d7b7358eb80d1edc086
Ronel <ronel.andaya@gmail.com>
2021-10-09 02:28:32 +0800

    Added error handling and complete handling on interval observable

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -21,6 +21,12 @@ export class HomeComponent implements OnInit, OnDestroy {
         let count = 0;
         setInterval(()=>{
           observer.next(count);
+          if(count === 2){
+            observer.complete();
+          }
+          if(count > 3){
+            observer.error(new Error('Count is greater than 3'));
+          }
           count++;
         },1000)
       }
@@ -28,6 +34,11 @@ export class HomeComponent implements OnInit, OnDestroy {
 
    this.firstObsSubscription =  customIntervalObservable.subscribe(data => {
       console.log(data)
+    },
+    error=>{
+      console.log(error.message);
+    },()=>{
+      console.log('Interval Complete');
     })
   }
 
